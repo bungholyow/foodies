12568da26234236ec009845231118297696e72aa
tommi <aritonang31031996@gmail.com>
2023-02-11 14:57:21 +0700

    file uploaded...

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>email kami</h3>
+         <a href="mailto:tommy@gmail.com">tommy@gmail.com</a>
+         <a href="mailto:tdedy@gmail.com">tdedy@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>jam buka</h3>
+         <p>00:07am ke 00:10pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>alamat kami</h3>
+         <a href="#">Jakarta, indonesia - 123321</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>hubungi kami</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+         <a href="tel:1112223333">111-222-3333</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>tommy</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
